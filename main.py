83205120a898c74d73df47225676f52aea74e534
Luiz Carvalho <luiz7fbc@gmail.com>
2022-11-08 16:14:46 -0300

    Developed Mesh class

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 from sirion import tdma1, tdma_2d
+from sirion import Mesh
 
 
-tdma_2d
+
+## MALHA
+nx = 5
+ny = 5
+a = 1
+b = 1
+## Expande malha para volumes fictícios
+# nxe = nx + 0
+# nye = ny + 0
+# # Nova origem dos eixos x,y
+# xe0 = a / nx
+# ye0 = b / ny
+
+# a = a + 2*xe0
+# b = b + 2*ye0
+# Cronstução da malha expandida
+mesh = Mesh(nx, ny, a, b)
+
+nodes = mesh.nodes
+volumes = mesh.elements
+neighbours = mesh.neighbours
+
+
+mesh.plot(anotate=True)
--- sirion/__init__.py
+++ sirion/__init__.py
@@ -1,2 +1,3 @@
 from .solver import tdma1
 from .solver import tdma_2d
+from .mesh import Mesh
